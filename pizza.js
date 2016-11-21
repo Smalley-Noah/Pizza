91b880bc14180343f459d7dfecab9254b4993e1d
Smalley-Noah <7nsmalley@stpatrick.org>
2016-11-21 11:19:38 -0600

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,28 @@
+class pizza{
+  //type constructor below
+constructor(s,mt,vt){
+  this.size = s; //array ["s","m","L","xL"]
+  this.meatToppings = mt;
+  this.veggietoppings = vt;
+}
+sizeCost(){
+  this.size(S) = 7.99
+  this.size(M) = 9.99
+  this.size(L) = 12.99
+  this.size(XL) = 15.99
+}
+toppingCost(top){
+  if(top == this.meatToppings){
+    return top.length*.99;
+  }
+  if(top == this.veggieToppings){
+    return top.length*.5;
+  }
+  price(){
+    return sizeCost + toppingCost[0] + toppingCost[1];
+  }
+  static promotionalDeal(pizza,percent){
+    let precent = 1-(percent/100);
+    return pizza.price()*percent;
+  }
+}
